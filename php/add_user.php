df9aac32041e921a01b9ef0907d8824cf08ff9c0
jrbarto <jrbarto@us.ibm.com>
2018-03-13 19:08:48 -0400

    First draft of website

---
+++ php/add_user.php
@@ -0,0 +1,27 @@
+ <?php
+$servername = "localhost";
+$username = "elephant";
+$password = "csuohio.edu";
+$dbname = "tuskdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+
+}
+
+$sql = "INSERT INTO Users (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+
+}
+
+$conn->close();
+?> 
---
+++ php/test.php
@@ -0,0 +1,4 @@
+<?php
+echo $_POST["site_user"];
+echo $_POST["site_pass"]; 
+?>
